247bbdb3c933e6e5f9c386ea5fd76d5799649632
kunerkhan.akerov <kunerkhan.akerov@yellow.systems>
2022-11-27 19:56:11 +0100

    Initial commit

---
+++ screens/About.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export const About = () => {
+    return (
+        <View style={styles.container}>
+            <Text>About Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    }
+})
